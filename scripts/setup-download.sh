38efbba9d0a1e81a1c24ec4dedb983cd632c9bf6
EulalieCoevoet <eulalie.coevoet@gmail.com>
2026-09-17 14:41:19 +0200

    Update git clone URLs to use HTTPS

--- scripts/setup-download.sh
+++ scripts/setup-download.sh
@@ -4,19 +4,19 @@
 # - python : Python build standalone from Astral (https://github.com/astral-sh/python-build-standalone/)
 
 # Clone sofa repository into src and use Compliance Robotics postinstall-fixup
-git clone git@github.com:sofa-framework/sofa.git src
-git clone git@github.com:SofaComplianceRobotics/Sofa.CI.Tools.git tools
+git clone https://github.com:sofa-framework/sofa.git src
+git clone https://github.com:SofaComplianceRobotics/Sofa.CI.Tools.git tools
 rm -r src/tools/postinstall-fixup
 mv tools/postinstall-fixup src/tools/postinstall-fixup
 
 # Clone plugins into the plugins directory
-git clone git@github.com:SofaComplianceRobotics/SofaPython3.git plugins/SofaPython3
-git clone git@github.com:sofa-framework/BeamAdapter.git plugins/BeamAdapter
-git clone --single-branch --branch robotics git@github.com:SofaComplianceRobotics/SofaGLFW.git plugins/SofaGLFW
-git clone git@github.com:SofaDefrost/SoftRobots.git plugins/SoftRobots
-git clone git@github.com:SofaDefrost/SoftRobots.Inverse.git plugins/SoftRobots.Inverse
-git clone git@github.com:SofaDefrost/Cosserat.git plugins/Cosserat
-git clone git@github.com:SofaDefrost/STLIB.git plugins/STLIB
+git clone https://github.com:SofaComplianceRobotics/SofaPython3.git plugins/SofaPython3
+git clone https://github.com:sofa-framework/BeamAdapter.git plugins/BeamAdapter
+git clone --single-branch --branch robotics https://github.com:SofaComplianceRobotics/SofaGLFW.git plugins/SofaGLFW
+git clone https://github.com:SofaDefrost/SoftRobots.git plugins/SoftRobots
+git clone https://github.com:SofaDefrost/SoftRobots.Inverse.git plugins/SoftRobots.Inverse
+git clone https://github.com:SofaDefrost/Cosserat.git plugins/Cosserat
+git clone https://github.com:SofaDefrost/STLIB.git plugins/STLIB
 
 # Create CMakelists for the plugins
 # If plugins/CMakeLists.txt exists, remove it
@@ -37,8 +37,12 @@ sofa_add_subdirectory(plugin SofaGLFW SofaGLFW ON)" >> plugins/CMakeLists.txt
 # Download Python
 # For Linux
 curl -L -o cpython.tar.gz https://github.com/astral-sh/python-build-standalone/releases/download/20260901/cpython-3.14.7+20260901-x86_64-unknown-linux-gnu-install_only.tar.gz
-tar -xvf cpython.tar.gz
+tar -xf cpython.tar.gz
 
 # For Windows
 
 # For MacOS
+
+echo "Working Directory:"
+ls 
+ls plugins
