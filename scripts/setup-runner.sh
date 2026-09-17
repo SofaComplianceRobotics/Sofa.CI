57fb2e4322f47b6bd0f693ea5fb415443ea778c5
EulalieCoevoet <eulalie.coevoet@gmail.com>
2026-09-17 15:11:16 +0200

    Add -y flag to apt install commands in setup-runner.sh

--- scripts/setup-runner.sh
+++ scripts/setup-runner.sh
@@ -1,16 +1,16 @@
 # Install build tools
-sudo apt install build-essential software-properties-common
-sudo apt install clang-14
-sudo apt install cmake cmake-gui
-sudo apt install ninja-build
-sudo apt install ccache
+sudo apt -y install build-essential software-properties-common
+sudo apt -y install clang-14
+sudo apt -y install cmake cmake-gui
+sudo apt -y install ninja-build
+sudo apt -y install ccache
 
 # Install dependencies
-sudo apt install libtinyxml2-dev
-sudo apt install libopengl0
-sudo apt install libboost-all-dev
+sudo apt -y install libtinyxml2-dev
+sudo apt -y install libopengl0
+sudo apt -y install libboost-all-dev
 python -m pip install numpy scipy pybind11==2.12.0
-sudo apt install libpng-dev libjpeg-dev libtiff-dev libglew-dev zlib1g-dev
-sudo apt install libeigen3-dev
+sudo apt -y install libpng-dev libjpeg-dev libtiff-dev libglew-dev zlib1g-dev
+sudo apt -y install libeigen3-dev
 
-sudo apt install xorg-dev libgtk-3-dev
+sudo apt -y install xorg-dev libgtk-3-dev
